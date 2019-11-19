168f6c00ed44e33ecf93a73e7cb993179d87da9b
Renny <rennyluzardo.fullstack@gmail.com>
2019-11-19 15:05:02 -0500

    setting global store with hooks and context

---
+++ config/store.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import rootReducer from '../reducers/rootReducer'
+export const Store = React.createContext()
+
+const initialState = {
+    products: []
+}
+
+const StoreProvider = (props) => {
+    const [state, dispatch] = React.useReducer(rootReducer, initialState)
+    const value = { state, dispatch }
+
+    return <Store.Provider value={value}>{props.children}</Store.Provider>
+}
+
+export default StoreProvider
