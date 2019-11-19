6e2bde8bf090c3362e0d27892f6251c32650f0b9
Renny <rennyluzardo.fullstack@gmail.com>
2019-11-19 15:04:23 -0500

    creating orders page

---
+++ pages/orders.jsx
@@ -0,0 +1,33 @@
+import { useContext, useEffect } from 'react'
+import { fetchProducts } from '../actions/product'
+import { Store } from '../config/store'
+import { Router } from '../routes'
+
+// Components
+import OrdersLayout from '../components/global/OrdersLayout'
+import '../scss/styles.scss'
+const Orders = props => {
+    const { state, dispatch } = useContext(Store)
+    const { router } = props
+
+    useEffect(() => {
+        state.products.length === 0 && fetchProducts(dispatch)
+
+        // effect
+
+        // return () => {
+        //     cleanup
+        // }
+    })
+
+    const goToHome = () => {
+        Router.pushRoute('index')
+    }
+
+    return (
+        <OrdersLayout>
+        </OrdersLayout>
+    )
+}
+
+export default Orders
