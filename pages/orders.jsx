0aa0706bcd08d8a9332ec2673fc4c8ace0cabe77
Renny <rennyluzardo.fullstack@gmail.com>
2019-11-20 18:21:13 -0500

    toolbar

--- pages/orders.jsx
+++ pages/orders.jsx
@@ -1,7 +1,10 @@
 import { useContext, useEffect } from 'react'
-import { fetchProducts } from '../actions/product'
+import { Row, Col, Icon, Button } from 'antd'
 import { Store } from '../config/store'
 import { Router } from '../routes'
+
+// Actions
+import { fetchProducts } from '../actions/product'
 
 // Components
 import OrdersLayout from '../components/layouts/OrdersLayout'
@@ -26,6 +29,35 @@ const Orders = props => {
 
     return (
         <OrdersLayout>
+            <div className="order-toolbar">
+                <Row>
+                    <Col span={15}>
+                        <div className="order-toolbar__alternative-box">
+                            <div className="order-toolbar__alternative-box--board-number">
+                                <Icon type="left" className="btn-back-toolbar" />
+                                <h3 className="text">Mesa 01</h3>
+                            </div>
+                            <div>
+                                {/* TODO: Counter Component */}
+                                <Row>
+                                    <Col span={4}>-</Col>
+                                    <Col span={16}>2 personas</Col>
+                                    <Col span={4}>+</Col>
+                                </Row>
+                            </div>
+                        </div>
+                    </Col>
+                    <Col span={9}>
+                        <div className="order-toolbar__right-actions">
+                            <Button className="order-toolbar__right-actions--1">T1</Button>
+                            <Button className="order-toolbar__right-actions--2">T2</Button>
+                            <Button className="order-toolbar__right-actions--3">T3</Button>
+                            <Button className="order-toolbar__right-actions--cancel">Cancelar Orden</Button>
+                        </div>
+                    </Col>
+                </Row>
+            </div>
+            <div>Products..</div>
         </OrdersLayout>
     )
 }
