e4ac97d4ee214f8e850057b3c5ceb9b303619a3a
Renny <rennyluzardo.fullstack@gmail.com>
2019-11-20 21:11:18 -0500

    top user name aligns

--- components/global/TopUserMenu.jsx
+++ components/global/TopUserMenu.jsx
@@ -4,13 +4,13 @@ import { Menu, Icon, Divider, Button } from 'antd'
 const TopUserMenu = () => {
     return (
         <ul className="top-user-menu">
-            <li key="username">
+            <li className="top-user-menu__username" key="username">
                 <Icon type="mail" /> <span>Renny</span>
             </li>
             <li className="custom-divider">
                 <Divider type="vertical" />
             </li>
-            <li className="top-user-menu__btn-top-hide"  key="hideBar">
+            <li className="top-user-menu__btn-top-hide" key="hideBar">
                 <span>ocultar</span> <div className="top-user-menu__btn-top-hide--arrow-hide-container">
                     <Button type="primary" shape="circle">
                         <Icon type="up" className="up-icon" />
