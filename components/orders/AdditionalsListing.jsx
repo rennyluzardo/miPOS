991f896605af04a64b4ac50e7c9155fdb2f8f031
Renny <rennyluzardo.fullstack@gmail.com>
2019-11-25 01:06:08 -0500

    integrations categories and products

--- components/orders/AdditionalsListing.jsx
+++ components/orders/AdditionalsListing.jsx
@@ -4,57 +4,15 @@ import { Col, Row } from 'antd'
 import Counter from '../global/Counter'
 
 const AdditionalsListing = props => {
-  const mockAdditionals = [
-    {
-      id: 53,
-      name: 'Arroz blanco'
-    },
-    {
-      id: 25,
-      name: 'Arroz Jardinera'
-    },
-    {
-      id: 51,
-      name: 'Papas fritas'
-    },
-    {
-      id: 4,
-      name: 'Papas al vapor'
-    },
-    {
-      id: 87,
-      name: 'Aguacate'
-    },
-    {
-      id: 72,
-      name: 'Platano al horno'
-    },
-    {
-      id: 124,
-      name: 'Yuca'
-    },
-    {
-      id: 545,
-      name: 'Yuca Frita'
-    },
-    {
-      id: 664,
-      name: 'Ensalada cocida'
-    },
-    {
-      id: 235,
-      name: 'Ensalada Cesar'
-    }
-  ]
   return (
     <div style={{ padding: '30px' }}>
       <Row gutter={[10, 20]}>
         {
-          mockAdditionals.map((additional, i) => {
+          !!props.specifications && props.specifications.map((specification, i) => {
             return <Col span={6} key={i}>
-              <div className="products-box" onClick={() => props.onSetAdditional(additional)}>
-                <div className="products-box__name">{additional.name}</div>
-                {additional.id === props.selectedAdditionals.id && <Counter isToolbar={false} />}
+              <div className="products-box" onClick={() => props.onSetAdditional(specification)}>
+                <div className="products-box__name">{specification.name}</div>
+                {specification.id === props.selectedAdditionals.id && <Counter isToolbar={false} />}
               </div>
             </Col>
           })
--- components/orders/CategoriesListing.jsx
+++ components/orders/CategoriesListing.jsx
@@ -1,49 +1,18 @@
 import { Card, Col, Row } from 'antd'
 
-const mockCategories = [
-  {
-    id: 1,
-    name: 'Desayunos',
-  },
-  {
-    id: 2,
-    name: 'Entradas',
-  },
-  {
-    id: 3,
-    name: 'Almuerzos',
-  },
-  {
-    id: 4,
-    name: 'Bebidas',
-  },
-  {
-    id: 5,
-    name: 'Vinos',
-  },
-  {
-    id: 6,
-    name: 'Postres',
-  },
-  {
-    id: 7,
-    name: 'Piqueo'
-  }
-]
-
 const CategoriesListing = props => {
   return (
     <div style={{ padding: '30px' }}>
       <Row gutter={[15, 30]}>
         {
-          mockCategories.map((category, i) => {
+          !!props.categories && props.categories.map((category, i) => {
             return <Col span={4} key={i}>
               <Card
-              bordered={false}
-              hoverable
-              cover={<img alt="img" src="" />}
-              className="categories-box"
-              onClick={() => props.onSetCategory(category)}>
+                bordered={false}
+                hoverable
+                cover={<img alt="img" src="" />}
+                className="categories-box"
+                onClick={() => props.onSetCategory(category)}>
                 {category.name}
               </Card>
             </Col>
