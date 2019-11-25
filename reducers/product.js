de1ef31856fcea4ee63af02d87953c7719288398
Renny <rennyluzardo.fullstack@gmail.com>
2019-11-25 01:06:42 -0500

    added categories reducer

--- reducers/product.js
+++ reducers/product.js
@@ -1,9 +1,11 @@
-import { FETCH_PRODUCTS } from '../actions/product'
+import { FETCH_PRODUCTS, FETCH_CATEGORIES } from '../actions/product'
 
 const productReducer = (state, action) => {
   switch (action.type) {
     case FETCH_PRODUCTS:
       return { ...state, products: action.payload }
+    case FETCH_CATEGORIES:
+      return { ...state, categories: action.payload }
     default:
       return state
   }
