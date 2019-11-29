2a5ac5793f6b5574330435692810f3881debb1ac
Renny <rennyluzardo.fullstack@gmail.com>
2019-11-29 15:47:03 -0500

    add product method with mock data

--- pages/orders.jsx
+++ pages/orders.jsx
@@ -5,7 +5,7 @@ import { Divider } from 'antd'
 import _ from 'lodash'
 // Actions
 import { fetchProducts, fetchCategories } from '../actions/product'
-import { setCart } from '../actions/cart'
+import { fetchSpot, addSpotProduct } from '../actions/spot'
 // Components
 import OrdersLayout from '../components/layouts/OrdersLayout'
 import CategoriesListing from '../components/orders/CategoriesListing'
@@ -20,96 +20,16 @@ import '../scss/styles.scss'
 const Orders = () => {
   const { state, dispatch } = useContext(Store)
 
+  const [hideTopBar, onHideTopBar] = useState(false)
   const [selectedCategory, setCategory] = useState({})
   const [selectedProduct, setProduct] = useState({})
   const [selectedAdditionals, setAdditional] = useState([])
   const [specifications, setSpecifications] = useState([])
   const [currentStep, setStep] = useState("CATEGORY")
   const [printTicketVisible, setPrintTicketVisible] = useState(true)
-
-  const mockCart = {
-    categories: [{
-      id: 109,
-      name: 'Hamburguesas',
-      products: [
-        {
-          "id": 681,
-          "name": "Hamburguesa de Carne",
-          "search_string": "hamburguesa de carne",
-          "description": null,
-          "base_value": 500,
-          "image": null,
-          "status": 1,
-          "invoice_name": "Hamburguesa de Carne",
-          "sku": null,
-          "ask_instruction": 0,
-          "eats_product_name": "Ninguno",
-          "image_version": 0,
-          "is_alcohol": 0,
-          "deleted_at": null,
-          "type_product": "null",
-          "nt_value": 446.4285714285714,
-          "category_name": "Hamburguesas",
-          "tax_values": "Object",
-          "specifications": "Array[1]",
-          "category": "Object",
-          "taxes": "Array[2]"
-        },
-        {
-          "id": 1717,
-          "name": "Hamburguer 2",
-          "search_string": "hamburguer 2",
-          "description": null,
-          "base_value": 20000,
-          "image": null,
-          "status": 1,
-          "invoice_name": "Hamburguer 2",
-          "sku": null,
-          "ask_instruction": 0,
-          "eats_product_name": "Ninguno",
-          "image_version": 0,
-          "is_alcohol": 0,
-          "deleted_at": null,
-          "type_product": "null",
-          "nt_value": 17857.142857142855,
-          "category_name": "Hamburguesas",
-          "tax_values": "Object",
-          "specifications": "Array[0]",
-          "category": "Object",
-          "taxes": "Array[0]"
-        }
-      ]
-    }, {
-      id: 142,
-      name: "Alitas",
-      products: [
-        {
-          "id": 1072,
-          "name": "Combo Alitas",
-          "search_string": "hamburgues",
-          "description": "hamburguesa 120",
-          "base_value": 2100000,
-          "image": null,
-          "status": 1,
-          "invoice_name": "Hamburgues",
-          "sku": "123344",
-          "ask_instruction": 1,
-          "eats_product_name": "Ninguno",
-          "image_version": 0,
-          "is_alcohol": 0,
-          "deleted_at": null,
-          "type_product": "null",
-          "nt_value": 1874999.9999999998,
-          "category_name": "Alitas",
-          "tax_values": "Object",
-          "specifications": "Array[14]",
-          "category": "Object",
-          "taxes": "Array[2]"
-        }
-      ]
-    }]
-  }
-
+  const [spotPlaces, setSpotPlaces] = useState(1)
+  const [windowDimensions, updateWindowDimensions] = useState({})
+  
   const handleOnBack = () => {
     switch (currentStep) {
       case "ADDITIONAL":
@@ -139,6 +59,7 @@ const Orders = () => {
       setStep("ADDITIONAL")
       setSpecifications(product.specifications)
     } else {
+      setProduct(product)
       handleOnAddProduct(product)
     }
   }
@@ -159,32 +80,118 @@ const Orders = () => {
   }
 
   const handleOnAddProduct = product => {
-    _.find(_.toArray(state.cart.categories), category => {
-      if (product.category_name !== category.name) {
-        const cart = _.cloneDeep(state.cart)
-        cart.categories.map(cat => {
-          if (cat.name === product.category_name) {
-            return cat.products.push(product)
-          } else {
-            // TODO: push product in other object with the new category
+    const mockProduct = {
+      "id_product": product.id,
+      "value": product.base_value,
+      "quantity": 1,
+      "name": product.name,
+      "id_spot": 290,
+      "instruction": "",
+      "invoice_name": product.invoice_name,
+      "specifications": [
+        {
+          "id": 850,
+          "name": "¿Deseas postre?",
+          "total": 2490000,
+          "options": [
+            {
+              "id": 4191,
+              "name": "Chocolate- chocolate personal",
+              "value": "1350000.0000",
+              "quantity": 0,
+              "checked": 0
+            },
+            {
+              "id": 4192,
+              "name": "Chocolate-chocolate súper personall",
+              "value": "2490000.0000",
+              "quantity": 1,
+              "checked": 1
+            },
+            {
+              "id": 4193,
+              "name": "Chocolate-chocolate Grande",
+              "value": "4780000.0000",
+              "quantity": 0,
+              "checked": 0
+            }
+          ],
+          "showQuantity": true,
+          "max": 3,
+          "required": 0
+        }
+      ]
+    }
+
+    console.log('Agregando producto')
+    addSpotProduct(dispatch, mockProduct).then(res => {
+      if (res.code === 200) {
+        console.log('Producto agregado')
+        fetchSpot(dispatch).then(res => {
+          if (res.code !== 200) {
+            console.log('Hubo un error al actualizar la mesa')
           }
         })
-        setCart(dispatch, cart)
+      } else {
+        console.log('Hubo un error al agregar el producto')
       }
     })
 
+    // Old method
+    // _.find(_.toArray(state.cart.categories), category => {
+    //   if (product.category_name !== category.name) {
+    //     const cart = _.cloneDeep(state.cart)
+    //     cart.categories.map(cat => {
+    //       if (cat.name === product.category_name) {
+    //         return cat.products.push(product)
+    //       } else {
+    //         // TODO: push product in other object with the new category
+    //       }
+    //     })
+    //     setCart(dispatch, cart)
+    //   }
+    // })
+  }
+
+  const handleOnAddPlace = () => {
+    let places = spotPlaces
+    places++
+    setSpotPlaces(places)
+  }
+
+  const handleOnRemovePlace = () => {
+    let places = spotPlaces
+    places--
+    setSpotPlaces(places)
+  }
+
+  const onWindowDimensions = () => {
+    updateWindowDimensions({ width: window.innerWidth, height: window.innerHeight })
   }
 
   useEffect(() => {
     _.isEmpty(state.categories) && fetchCategories(dispatch)
-    _.isEmpty(state.cart) && setCart(dispatch, mockCart)
+    _.isEmpty(state.spot) && fetchSpot(dispatch)
   })
+
+  useEffect(() => {
+    _.isEmpty(windowDimensions) && onWindowDimensions()
+    window.addEventListener('resize', onWindowDimensions)
+
+    return () => {
+      window.removeEventListener('resize', onWindowDimensions)
+    }
+  }, [windowDimensions])
 
   const orderToolbarProps = {
     onBack: handleOnBack,
     selectedCategory: selectedCategory,
     selectedProduct: selectedProduct,
-    currentStep: currentStep
+    currentStep: currentStep,
+    spotPlaces: spotPlaces,
+    onAddPlace: handleOnAddPlace,
+    onRemovePlace: handleOnRemovePlace,
+    windowDimensions: windowDimensions
   }
 
   const modalPrintTicketProps = {
@@ -192,17 +199,21 @@ const Orders = () => {
     visible: printTicketVisible
   }
 
+  const ordersLayoutProps = {
+    goToHome: goToHome,
+    spot: state.spot,
+    hideTopBar: hideTopBar,
+    onHideTopBar: onHideTopBar,
+    spotPlaces: spotPlaces
+  }
+
   return (
-    <OrdersLayout goToHome={goToHome} >
+    <OrdersLayout {...ordersLayoutProps}>
       <OrderToolbar {...orderToolbarProps} />
       <div className="toolbar-divider">
         <Divider />
       </div>
       <div className="content-container">
-        {
-          currentStep === "ADDITIONAL" &&
-          <MotiveSelector />
-        }
         {
           currentStep === "CATEGORY" &&
           <CategoriesListing
@@ -218,10 +229,11 @@ const Orders = () => {
         }
         {
           currentStep === "ADDITIONAL" &&
+          [<MotiveSelector />,
           <AdditionalsListing
             onSetAdditional={handleOnSetAdditional}
             specifications={specifications}
-            selectedAdditionals={selectedAdditionals} />
+            selectedAdditionals={selectedAdditionals} />]
         }
       </div>
       <ModalPrintTicket {...modalPrintTicketProps} />
