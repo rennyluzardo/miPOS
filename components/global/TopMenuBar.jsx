8f2cf3d13b7259bd5b53f2fdd4f34da9e6bb84e0
Renny <rennyluzardo.fullstack@gmail.com>
2019-11-30 21:33:46 -0500

    top menu bar icons

--- components/global/TopMenuBar.jsx
+++ components/global/TopMenuBar.jsx
@@ -13,37 +13,37 @@ const TopMenuBar = () => {
             <Menu.Item
                 key="orders"
                 onClick={() => selectMenu('orders')}>
-                <img src={images.topMenu.ico1} alt="" />
+                <div className={selectedMenu === 'orders' && 'icon'}><img src={images.topMenu.ico1} alt="" /></div>
                 {selectedMenu === 'orders' && ' Ordenes'}
             </Menu.Item>
             <Menu.Item
                 key="Menu2"
                 onClick={() => selectMenu('Menu2')}>
-                <img src={images.topMenu.ico2} alt="" />
+                <div className={selectedMenu === 'Menu2' && 'icon'}><img src={images.topMenu.ico2} alt="" /></div>
                 {selectedMenu === 'Menu2' && ' Menu2'}
             </Menu.Item>
             <Menu.Item
                 key="Menu3"
                 onClick={() => selectMenu('Menu3')}>
-                <img src={images.topMenu.ico3} alt="" />
+                <div className={selectedMenu === 'Menu3' && 'icon'}><img src={images.topMenu.ico3} alt="" /></div>
                 {selectedMenu === 'Menu3' && ' Menu3'}
             </Menu.Item>
             <Menu.Item
                 key="Menu4"
                 onClick={() => selectMenu('Menu4')}>
-                <img src={images.topMenu.ico4} alt="" />
+                <div className={selectedMenu === 'Menu4' && 'icon'}><img src={images.topMenu.ico4} alt="" /></div>
                 {selectedMenu === 'Menu4' && ' Menu4'}
             </Menu.Item>
             <Menu.Item
                 key="Menu5"
                 onClick={() => selectMenu('Menu5')}>
-                <img src={images.topMenu.ico5} alt="" />
+                <div className={selectedMenu === 'Menu5' && 'icon'}><img src={images.topMenu.ico5} alt="" /></div>
                 {selectedMenu === 'Menu5' && ' Menu5'}
             </Menu.Item>
             <Menu.Item
                 key="Menu6"
                 onClick={() => selectMenu('Menu6')}>
-                <img src={images.topMenu.ico6} alt="" />
+                <div className={selectedMenu === 'Menu6' && 'icon'}><img src={images.topMenu.ico6} alt="" /></div>
                 {selectedMenu === 'Menu6' && ' Menu6'}
             </Menu.Item>
         </Menu>
