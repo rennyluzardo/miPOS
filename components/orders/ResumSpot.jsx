e96e0d2d9e4f7faea8f8360c99647347141fddc2
Renny <rennyluzardo.fullstack@gmail.com>
2019-12-01 06:39:31 -0500

    generals fixes in resum box

---
+++ components/orders/ResumSpot.jsx
@@ -0,0 +1,25 @@
+import { Row, Col } from 'antd'
+import { centsToDollar } from '../../lib/utils'
+
+const ResumSpot = props => {
+  return (
+    <Row className="product-cart">
+      {
+        !!props.products && props.products.map((spot, i) => {
+          return [<Col span={24} key={i}>
+            <h3 className="product-cart__title">Categoria</h3>
+          </Col>,
+          <Col span={24} key={spot}>
+            <div className="product-cart__item" onClick={() => props.onSelectEditProduct(spot)}>
+              <div className="product-cart__item--qty">{spot.quantity}</div>
+              <div className="product-cart__item--name">{spot.name_product}</div>
+              <div className="product-cart__item--price">$ {centsToDollar(spot.nt_value).toFixed(2)}</div>
+            </div>
+          </Col>]
+        })
+      }
+    </Row>
+  )
+}
+
+export default ResumSpot
