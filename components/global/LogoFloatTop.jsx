f675df87f2c5031306ad4427308c6c240f957cec
Renny <rennyluzardo.fullstack@gmail.com>
2019-11-30 21:32:07 -0500

    setting hover event and styles in logo float

--- components/global/LogoFloatTop.jsx
+++ components/global/LogoFloatTop.jsx
@@ -1,9 +1,19 @@
+import { useState } from 'react'
 import { Icon } from 'antd'
 
 const LogoFloatTop = props => {
+    const [hover, setHover] = useState(false)
+
     return (
-        <div className={`logo-float-visible ${props.hide ? 'logo-float-hide' : ''}`} onClick={props.onHideTopBar}>
-            <img src="static/images/logo/miPOS-Circle.png" alt=""/>
+        <div className={`logo-float-visible ${props.hide ? 'logo-float-hide' : ''}`}>
+            <div className="logo-float-bg"
+                onMouseOver={() => setHover(true)}
+                onMouseLeave={() => setHover(false)}>
+                <img src="static/images/logo/miPOS-Circle.png" alt="" />
+                <span
+                    className={`logo-float-text ${hover ? 'show-logo-float-text' : ''}`}
+                    onClick={props.onHideTopBar}>mostrar</span>
+            </div>
         </div>
     )
 }
