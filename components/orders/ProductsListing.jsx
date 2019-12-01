af9684fd30db6b3aa7375b7a5da10d81f5e9d0ce
Renny <rennyluzardo.fullstack@gmail.com>
2019-12-01 06:39:54 -0500

    product listing counter props

--- components/orders/ProductsListing.jsx
+++ components/orders/ProductsListing.jsx
@@ -1,6 +1,20 @@
 import { Col, Row } from 'antd'
+import _ from 'lodash'
+import { centsToDollar } from '../../lib/utils.js'
+// Components
+import CounterProduct from '../global/CounterProduct'
 
 const ProductsListing = props => {
+  const counterProductProps = {
+    selectedProduct: props.selectedProduct,
+    spotPlaces: props.spotPlaces,
+    windowDimensions: props.windowDimensions,
+    onCounter: props.onCounter,
+    operationLeft: 'removeProduct',
+    operationRight: 'addProduct',
+    qtyProduct: props.qtyProduct
+  }
+
   return (
     <div style={{ padding: '30px' }}>
       <Row gutter={[10, 20]}>
@@ -9,8 +23,21 @@ const ProductsListing = props => {
             return <Col span={4} className="items-container-x5" key={i}>
               <div className="products-box" onClick={() => props.onSetProduct(product)}>
                 <div className="products-box__name">{product.name}</div>
-                <div className="products-box__price">$ {product.nt_value.toFixed(2)}</div>
+                {
+                  !!props.selectedProduct &&
+                  !!props.selectedProduct.product_detail &&
+                  props.selectedProduct.product_detail.product_id === product.id &&
+                  <CounterProduct {...counterProductProps} />
+                }
               </div>
+              {
+                !!props.selectedProduct &&
+                  !!props.selectedProduct.id &&
+                  props.selectedProduct.id === product.id &&
+                  _.isEmpty(props.selectedProduct.specifications)
+                  ? <CounterProduct {...counterProductProps} />
+                  : <div className="products-box__price">$ {centsToDollar(product.nt_value).toFixed(2)}</div>
+              }
             </Col>
           })
         }
