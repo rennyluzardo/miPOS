f0b0af5d2db10336bb4b2269b5e3d8aa434927b6
Renny <rennyluzardo.fullstack@gmail.com>
2019-12-01 06:34:03 -0500

    cart reducer

--- reducers/rootReducer.js
+++ reducers/rootReducer.js
@@ -1,15 +1,18 @@
 import productReducer from './product'
 import spotReducer from './spot'
+import cartReducer from './cart'
 
 const rootReducer = (state, type) => {
-    switch (type.reducer) {
-        case 'products':
-            return productReducer(state, type)
-        case 'spot':
-            return spotReducer(state, type)
-        default:
-            return state
-    }
+  switch (type.reducer) {
+    case 'products':
+      return productReducer(state, type)
+    case 'spot':
+      return spotReducer(state, type)
+    case 'cart':
+      return cartReducer(state, type)
+    default:
+      return state
+  }
 }
 
 export default rootReducer
