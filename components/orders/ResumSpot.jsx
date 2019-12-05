e5430f60b2f6b6b5046d98e4225c7816517cf036
Renny <rennyluzardo.fullstack@gmail.com>
2019-12-05 02:46:16 -0500

    style img element in delivery button

--- components/orders/ResumSpot.jsx
+++ components/orders/ResumSpot.jsx
@@ -4,7 +4,8 @@ import { centsToDollar } from '../../lib/utils'
 const ResumSpot = props => {
   return (
     <Row className="product-cart">
-      {
+      {/* Template agrupados por categoria */}
+      {/* {
         !!props.spotProducts && props.spotProducts.map((spot, i) => {
           return [<Col span={24} key={i}>
             <h3 className="product-cart__title">Categoria</h3>
@@ -16,6 +17,18 @@ const ResumSpot = props => {
               <div className="product-cart__item--price">$ {centsToDollar(spot.nt_value).toFixed(2)}</div>
             </div>
           </Col>]
+        })
+      } */}
+
+      {
+        !!props.spotProducts && props.spotProducts.map((spot, i) => {
+          return <Col span={24} key={spot}>
+            <div className="product-cart__item" onClick={() => props.onSelectEditProduct(spot)}>
+              <div className="product-cart__item--qty">{spot.quantity}</div>
+              <div className="product-cart__item--name">{spot.name_product}</div>
+              <div className="product-cart__item--price">$ {centsToDollar(spot.nt_value).toFixed(2)}</div>
+            </div>
+          </Col>
         })
       }
     </Row>
